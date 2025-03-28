ab8f95c9c0c89b3bd89ab667496b9134c315dda2
Eliseo Murillo <eliseomurillo9@gmail.com>
2025-03-28 01:02:01 +0100

    fix: clean architecture applied

--- core/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import Category
-from .models import Article
-from .models import GeneratedImage
-
-admin.site.register(Category)
-admin.site.register(Article)
-admin.site.register(GeneratedImage)
