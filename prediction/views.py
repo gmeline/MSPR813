a8d9b6fda2ebcaedc9b382a30d8f339b099d9574
Genessy <thibault.scorielle@gmail.com>
2025-04-16 19:00:26 +0200

    feat: Implémentation de la page de choix du modèle

--- prediction/views.py
+++ prediction/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render
+import joblib
+import os
 
 # Create your views here.
+def choix(request):
+    return render(request, 'choix/index.html')
+
 def prediction(request):
-    return render(request, 'prediction/index.html')
+    prediction_result = None
+    if request.method == 'POST':
+        data = [[
+            float(request.POST.get('precedent')),
+            float(request.POST.get('tauxpourmille')),
+            float(request.POST.get('tauxdechomage')),
+            float(request.POST.get('densite')),
+            float(request.POST.get('instabilitecentreextreme')),
+            float(request.POST.get('poidsnuancecentreextremes')),
+            float(request.POST.get('desirchangementcentreextremes')),
+        ]]
+        model_path = os.path.join("prediction", "model_with_center_previous_instability_criminality_unemployment_density")
+        model = joblib.load(model_path)
+        prediction_result = model.predict(data)[0]
+    return render(request, 'prediction/index.html', {'prediction': prediction_result})
