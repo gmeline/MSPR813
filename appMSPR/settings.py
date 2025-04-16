4405e90ad14f0ed4718aa65f2137ace7b4d50c37
Genessy <thibault.scorielle@gmail.com>
2025-04-16 09:48:12 +0200

    Renommage de la feature

--- appMSPR/settings.py
+++ appMSPR/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "dashboard",
-    "landing",
+    "prediction",
     "machineLearning",
     "rest_framework",
     "matplotlib",
--- landing/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from landing import views
-
-urlpatterns = [
-    path('', views.landing, name='landing'),
-]
