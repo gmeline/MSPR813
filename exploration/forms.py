03693a0a4f972d2d73f70e67368475698dbfef4d
gmeline <melinegodefroy.pr@gmail.com>
2025-04-29 21:27:33 +0200

    Ajout du dump et des cases a cocher

--- exploration/forms.py
+++ exploration/forms.py
@@ -12,7 +12,7 @@ class ElectionFilterForm(forms.Form):
         widget=forms.Select(attrs={'class': 'form-control'})
     )
     # Filtrer par nuance
-    nuances=df['nuance'].unique()
+    nuances = df['nuance'].unique()
     nuance_choices = [(nuan, nuan) for nuan in nuances]
     nuance_field = forms.ChoiceField(
         choices=[('', 'Sélectionner une nuance')] + nuance_choices,
@@ -20,6 +20,45 @@ class ElectionFilterForm(forms.Form):
         widget=forms.Select(attrs={'class': 'form-control'})
     )
 
+    # Checkbox pour affichage de colonnes
+    show_code_du_departement = forms.BooleanField(required=False, initial=False)
+    show_libelle_du_departement = forms.BooleanField(required=False, initial=True)
+    show_code_de_la_circonscription = forms.BooleanField(required=False, initial=True)
+    show_inscrits = forms.BooleanField(required=False, initial=True)
+    show_volants = forms.BooleanField(required=False, initial=True)
+    show_exprimees = forms.BooleanField(required=False, initial=True)
+    show_blancs_et_nuls = forms.BooleanField(required=False, initial=True)
+    show_nuance = forms.BooleanField(required=False, initial=True)
+    show_voix = forms.BooleanField(required=False, initial=True)
+    show_nuance_2 = forms.BooleanField(required=False, initial=True)
+    show_voix_2 = forms.BooleanField(required=False, initial=True)
+    show_nuance_3 = forms.BooleanField(required=False, initial=True)
+    show_voix_3 = forms.BooleanField(required=False, initial=True)
+    show_annee = forms.BooleanField(required=False, initial=True)
+
+    show_gagnant = forms.BooleanField(required=False, initial=True)
+    show_voix_gagnant = forms.BooleanField(required=False, initial=True)
+    show_gagnant_precedent = forms.BooleanField(required=False, initial=True)
+    show_voix_gagnant_precedent = forms.BooleanField(required=False, initial=True)
+
+    show_encodage_sans_centre_gagnant = forms.BooleanField(required=False, initial=True)
+    show_encodage_avec_centre_gagnant = forms.BooleanField(required=False, initial=True)
+    show_encodage_centre_extremes_gagnant = forms.BooleanField(required=False, initial=True)
+    show_encodage_sans_centre_gagnant_precedent = forms.BooleanField(required=False, initial=True)
+    show_encodage_avec_centre_gagnant_precedent = forms.BooleanField(required=False, initial=True)
+    show_encodage_centre_extremes_gagnant_precedent = forms.BooleanField(required=False, initial=True)
+
+    show_instabilite_sans_centre = forms.BooleanField(required=False, initial=True)
+    show_poids_nuance_sans_centre = forms.BooleanField(required=False, initial=True)
+    show_desir_changement_sans_centre = forms.BooleanField(required=False, initial=True)
+
+    show_instabilite_avec_centre = forms.BooleanField(required=False, initial=True)
+    show_poids_nuance_avec_centre = forms.BooleanField(required=False, initial=True)
+    show_desir_changement_avec_centre = forms.BooleanField(required=False, initial=True)
+
+    show_instabilite_centre_extremes = forms.BooleanField(required=False, initial=True)
+    show_poids_nuance_centre_extremes = forms.BooleanField(required=False, initial=True)
+    show_desir_changement_centre_extremes = forms.BooleanField(required=False, initial=True)
 
 class ImportCSVForm(forms.Form):
     csv_file = forms.FileField()
--- exploration/views.py
+++ exploration/views.py
@@ -1,83 +1,126 @@
 from django.shortcuts import render
 from .forms import ElectionFilterForm
+from .models import ResultatElection
 import matplotlib
-matplotlib.use('Agg') 
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
-import pandas as pd
 import os
 from django.conf import settings
+from django.db import models
+import pandas as pd
+
 
 def index(request):
-    csv_path = os.path.join(settings.BASE_DIR, 'core', 'data', 'leg_1993.csv')
+    csv_path = os.path.join(settings.BASE_DIR, 'exploration', 'data', 'leg_1993.csv')
     
     if not os.path.exists(csv_path):
         return render(request, 'exploration/index.html', {
             'error': 'Le fichier CSV est introuvable.',
         })
+    
     df = pd.read_csv(csv_path, sep=',')
     departements = df['libelle_du_departement'].unique()
     nuances = df['nuance'].unique()
-
-    # Création du formulaire
     form = ElectionFilterForm(request.GET)
 
     if form.is_valid():
-        # Récupérer les filtres
         selected_department = form.cleaned_data.get('department_field')
         selected_nuance = form.cleaned_data.get('nuance_field')
 
-        # Filtrer les données par département
         if selected_department:
             df = df[df['libelle_du_departement'] == selected_department]
+        
         if selected_nuance:
             df = df[df['nuance'] == selected_nuance]
 
-    # Liste vide pour les dates, car il n'y a pas de colonne 'Date'
-    dates_uniques = []
+        show_code_du_departement = form.cleaned_data.get('show_code_du_departement')
+        show_libelle_du_departement = form.cleaned_data.get('show_libelle_du_departement')
+        show_code_de_la_circonscription = form.cleaned_data.get('show_code_de_la_circonscription')
+        show_inscrits = form.cleaned_data.get('show_inscrits')
+        show_volants = form.cleaned_data.get('show_volants')
+        show_exprimees = form.cleaned_data.get('show_exprimees')
+        show_blancs_et_nuls = form.cleaned_data.get('show_blancs_et_nuls')
+        show_nuance = form.cleaned_data.get('show_nuance')
+        show_voix = form.cleaned_data.get('show_voix')
+        show_nuance_2 = form.cleaned_data.get('show_nuance_2')
+        show_voix_2 = form.cleaned_data.get('show_voix_2')
+        show_nuance_3 = form.cleaned_data.get('show_nuance_3')
+        show_voix_3 = form.cleaned_data.get('show_voix_3')
+        show_annee = form.cleaned_data.get('show_annee')
+        show_gagnant = form.cleaned_data.get('show_gagnant')
+        show_voix_gagnant = form.cleaned_data.get('show_voix_gagnant')
+        show_gagnant_precedent = form.cleaned_data.get('show_gagnant_precedent')
+        show_voix_gagnant_precedent = form.cleaned_data.get('show_voix_gagnant_precedent')
+        show_encodage_sans_centre_gagnant = form.cleaned_data.get('show_encodage_sans_centre_gagnant')
+        show_encodage_avec_centre_gagnant = form.cleaned_data.get('show_encodage_avec_centre_gagnant')
+        show_encodage_centre_extremes_gagnant = form.cleaned_data.get('show_encodage_centre_extremes_gagnant')
+        show_encodage_sans_centre_gagnant_precedent = form.cleaned_data.get('show_encodage_sans_centre_gagnant_precedent')
+        show_encodage_avec_centre_gagnant_precedent = form.cleaned_data.get('show_encodage_avec_centre_gagnant_precedent')
+        show_encodage_centre_extremes_gagnant_precedent = form.cleaned_data.get('show_encodage_centre_extremes_gagnant_precedent')
+        show_instabilite_sans_centre = form.cleaned_data.get('show_instabilite_sans_centre')
+        show_poids_nuance_sans_centre = form.cleaned_data.get('show_poids_nuance_sans_centre')
+        show_desir_changement_sans_centre = form.cleaned_data.get('show_desir_changement_sans_centre')
+        show_instabilite_avec_centre = form.cleaned_data.get('show_instabilite_avec_centre')
+        show_poids_nuance_avec_centre = form.cleaned_data.get('show_poids_nuance_avec_centre')
+        show_desir_changement_avec_centre = form.cleaned_data.get('show_desir_changement_avec_centre')
+        show_instabilite_centre_extremes = form.cleaned_data.get('show_instabilite_centre_extremes')
+        show_poids_nuance_centre_extremes = form.cleaned_data.get('show_poids_nuance_centre_extremes')
+        show_desir_changement_centre_extremes = form.cleaned_data.get('show_desir_changement_centre_extremes')
 
-    # Si df est vide, on définit 'data' comme une liste vide
     data = df.values.tolist() if not df.empty else []
 
     if not df.empty:
-        # Vérifier si la colonne 'voix' existe
         if 'voix' not in df.columns:
             return render(request, 'exploration/index.html', {
                 'error': 'La colonne "voix" est manquante dans le fichier CSV.',
             })
         
-        # Nettoyage des voix pour s'assurer qu'elles sont bien sous forme numérique
         df['voix'] = pd.to_numeric(df['voix'], errors='coerce')
-
-        # Vérifier si des valeurs ont été converties en NaN et les supprimer
         df = df.dropna(subset=['voix'])
-
-        # Regrouper les données par nuance (candidat ou parti) et somme des voix
-        voix_par_candidat = df.groupby('nuance')['voix'].sum().sort_values(ascending=False)
-
-        # Rotation des labels pour une meilleure lisibilité
-        plt.xticks(rotation=90)
-
-        # Définir le chemin de l'image
-        image_path = os.path.join(settings.BASE_DIR, 'static', 'images', 'tableau.png')
-
-        # Créer le répertoire si nécessaire
-        os.makedirs(os.path.dirname(image_path), exist_ok=True)
-
-        # Retourner la réponse avec les données et l'image du graphique
         return render(request, 'exploration/index.html', {
             'form': form,
-            'dates': dates_uniques,  # Liste vide, puisque la colonne 'Date' n'existe pas
             'data': data,
-            'graph_image_url': '/static/images/tableau.png', 
-            'departements': departements,  # Passer la liste des départements
-            'nuances': nuances
+            'show_code_du_departement': show_code_du_departement,
+            'show_libelle_du_departement': show_libelle_du_departement,
+            'show_code_de_la_circonscription': show_code_de_la_circonscription,
+            'show_inscrits': show_inscrits,
+            'show_volants': show_volants,
+            'show_exprimees': show_exprimees,
+            'show_blancs_et_nuls': show_blancs_et_nuls,
+            'show_nuance': show_nuance,
+            'show_voix': show_voix,
+            'show_nuance_2': show_nuance_2,
+            'show_voix_2': show_voix_2,
+            'show_nuance_3': show_nuance_3,
+            'show_voix_3': show_voix_3,
+            'show_annee': show_annee,
+            'show_gagnant': show_gagnant,
+            'show_voix_gagnant': show_voix_gagnant,
+            'show_gagnant_precedent': show_gagnant_precedent,
+            'show_voix_gagnant_precedent': show_voix_gagnant_precedent,
+            'show_encodage_sans_centre_gagnant': show_encodage_sans_centre_gagnant,
+            'show_encodage_avec_centre_gagnant': show_encodage_avec_centre_gagnant,
+            'show_encodage_centre_extremes_gagnant': show_encodage_centre_extremes_gagnant,
+            'show_encodage_sans_centre_gagnant_precedent': show_encodage_sans_centre_gagnant_precedent,
+            'show_encodage_avec_centre_gagnant_precedent': show_encodage_avec_centre_gagnant_precedent,
+            'show_encodage_centre_extremes_gagnant_precedent': show_encodage_centre_extremes_gagnant_precedent,
+            'show_instabilite_sans_centre': show_instabilite_sans_centre,
+            'show_poids_nuance_sans_centre': show_poids_nuance_sans_centre,
+            'show_desir_changement_sans_centre': show_desir_changement_sans_centre,
+            'show_instabilite_avec_centre': show_instabilite_avec_centre,
+            'show_poids_nuance_avec_centre': show_poids_nuance_avec_centre,
+            'show_desir_changement_avec_centre': show_desir_changement_avec_centre,
+            'show_instabilite_centre_extremes': show_instabilite_centre_extremes,
+            'show_poids_nuance_centre_extremes': show_poids_nuance_centre_extremes,
+            'show_desir_changement_centre_extremes': show_desir_changement_centre_extremes,
+            'departements': departements,
+            'nuances': nuances,
+            'graph_image_url': '/static/images/tableau.png',
         })
 
-    # Si df est vide, retourner une réponse avec une liste vide pour 'data'
     return render(request, 'exploration/index.html', {
         'form': form,
-        'dates': dates_uniques,  # Liste vide
         'data': data,
-        'departements': departements,  # Passer la liste des départements
-        'naunces': nuances
+        'departements': departements,
+        'nuances': nuances,
     })
